45725887b09b0700adf767893b9079b71816a7c9
thai thien <thien.thai.meow@gmail.com>
2025-02-22 05:22:38 +0700

    draw dog

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
-if __name__ == "__main__":
+def draw_ascii_dog():
+    """
+    Generates and prints an ASCII representation of a dog.
+    """
+    dog = """
+     /\_/\
+    ( o.o )
+     >" "<
+         ||
+         ==
+    """
+    print(dog)
+
+
+f __name__ == "__main__":
     print("meow")
